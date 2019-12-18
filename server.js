add8432b36cb588c84aac9c5167b74d5e026b726
nikolaz-fauzi <nikolazfauzi@yahoo.co.id>
2019-12-18 23:11:05 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+
+// Import typedefs and resolver
+const filePath = path.join(__dirname, "typeDefs.gql");
+const typeDefs = fs.readFileSync(filePath, "utf-8");
+const resolvers = require("./resolvers");
+
+// import environment variables and mongoose models
+require("dotenv").config({ path: "variables.env" });
+const User = require("./models/User");
+const Post = require("./models/Post");
+
+// Connect to mongoose cloud atlas
+mongoose
+  .connect(process.env.MONGO_URI, { useUnifiedTopology:true, useNewUrlParser: true })
+  .then(() => console.log("DB CONNECTED"))
+  .catch(err => console.log(err));
+
+  // create apollo/graphQL server using typedefs, resolver, context
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: {
+    User,
+    Post
+  }
+});
+
+// listening, default port 4000
+server.listen(4001).then(({ url }) => {
+  console.log(`Server listening ${url}`);
+});
