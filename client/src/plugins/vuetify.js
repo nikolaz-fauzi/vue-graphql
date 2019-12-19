e4ee3b6fe33fca57e26f1a232674421164b23da2
Nikolaz Fauzi <nikolaz.fauzi@lab.futuready.com>
2019-12-19 14:09:45 +0700

    21

---
+++ client/src/plugins/vuetify.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#26A69A",
+        secondary: "#80CBC4",
+        accent: "#EF5350",
+        error: "#D50000",
+        warning: "#FFC107",
+        info: "#2979FF",
+        success: "#1B5E20"
+      }
+    }
+  }
+});
